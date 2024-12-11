8eafc85962933ebafb14ca7591419c672da095c2
Sergey Grigorash <sergey.adviser@gmail.com>
2024-12-11 09:46:20 +0700

    feat add IE11 support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import viteLegacyPlugin from "@vitejs/plugin-legacy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteLegacyPlugin({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
   base: "idea_platform_test_grigorash",
 });
